48cbce8b37655105ed7aa8b269c2305b7c4d8cd8
Sibylle Gehring <sibylle.gehring@hotmail.com>
2022-06-30 11:17:42 +0200

    remove @getter main

--- src/main/java/ch/bbw/squid/SquidApplication.java
+++ src/main/java/ch/bbw/squid/SquidApplication.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Getter
 @SpringBootApplication
 public class SquidApplication {
 
