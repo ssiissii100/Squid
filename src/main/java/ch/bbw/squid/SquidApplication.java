96ed42c20d7c3b4603e54ef05dc4815b3bf93693
Sibylle Gehring <sibylle.gehring@hotmail.com>
2022-06-30 08:04:43 +0200

    fresh repo

---
+++ src/main/java/ch/bbw/squid/SquidApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.squid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquidApplication.class, args);
+    }
+
+}
